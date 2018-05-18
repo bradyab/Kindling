80f2050e19727ca5356cf36db8f82f0e5261aa81
Christopher Hurt <topherhurt@gmail.com>
2018-05-18 12:15:47 -0400

    Fixed null pixmap error when the training pictures run out

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
 # exec(open("gui.py").read(), globals())
 import sys
+from foldercount import numfile
+
+import gui
 
 exec(open('gatherimages.py').read())
 
-sys.argv = ['gui.py','train']
-exec(open('gui.py').read())
+train_q = gui.QApplication([])
+train_app = gui.App('train')
+# Continue training while there are images left in /images
+while numfile('./training_data/like') + numfile('./training_data/dislike') < numfile('./images/'):
+	train_app.photoChange('./images/')
+train_q.quit()
 
-sys.argv = ['gui.py','predict']
-exec(open('gui.py').read())
+predict_q = gui.QApplication([])
+predict_app = gui.App('predict')
+
+# sys.argv = ['gui.py','train']
+# exec(open('gui.py').read())
+
+# sys.argv = ['gui.py','predict']
+# exec(open('gui.py').read())
